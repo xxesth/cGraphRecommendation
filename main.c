e3f094a7176acad17096efc0a15bfdd8e46eb9d2
xesth <muhammedali.karsli1@gmail.com>
2024-11-02 21:57:12 +0000

    adding c code

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "graph.h"
+
+int main() {
+    Graph *graph = createGraph();
+    parseDataFile(graph, "./ml-100k/u.data"); // Replace with your actual data file path
+
+    printGraph(graph);
+    
+
+    // Free allocated memory and clean up (not shown)
+    freeGraph(graph);
+
+    return 0;
+}
+
