559a506403b6837904e6981174e57a5f3faf2621
xesth <muhammedali.karsli1@gmail.com>
2024-11-29 00:08:01 +0300

    made till 4th. 4th is implemented but does not function. fix then continue.

--- main.c
+++ main.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 #include "graph.h"
 
+#define DATA_ADDRESS "./ml-100k/u.data"
+#define RECOMMEND_TO_USER 53
+#define NUMBER_OF_MOVIES 5
+
 int main() {
     Graph *graph = createGraph();
-    parseDataFile(graph, "./ml-100k/u.data"); // Replace with your actual data file path
+    parseDataFile(graph, DATA_ADDRESS); // Replace with your actual data file path
 
     printGraph(graph);
     
+    //TEST
+    //Node *user = findNode(graph->users, RECOMMEND_TO_USER);
+    //printEdges(user);
+    
+    //Recommendations
+    recommendRandomMovie(graph, RECOMMEND_TO_USER);
+    recommendTopRatedMovies(graph, RECOMMEND_TO_USER, NUMBER_OF_MOVIES);
+    recommendBasedOnSimilarUser(graph, RECOMMEND_TO_USER, NUMBER_OF_MOVIES);
+    recommendClosestMovies(graph, RECOMMEND_TO_USER, NUMBER_OF_MOVIES);
 
-    // Free allocated memory and clean up (not shown)
-//    freeGraph(graph);
+    freeGraph(graph);
 
     return 0;
 }
-
