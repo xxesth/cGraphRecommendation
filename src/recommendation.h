201ff70dee5a250103b4823160135cd280847b85
xesth <muhammedali.karsli1@gmail.com>
2024-12-03 23:13:15 +0300

    demo of neural network

--- src/recommendation.h
+++ src/recommendation.h
@@ -2,14 +2,12 @@
 #define RECOMMENDATION_H
 
 #include "graph.h"
-#include "neuralnetwork.h"
 
 void recommendRandomMovie(Graph *graph, int userId);
 void recommendTopRatedMovies(Graph *graph, int userId, int n);
 void recommendBasedOnSimilarUser(Graph *graph, int userId, int n);
 void recommendClosestMovies(Graph *graph, int userId, int n);
 void oldrecommendClosestMovies(Graph *graph, int userId, int n);
-void recommendMovies(NeuralNetwork *nn, int userId, int n, Graph *graph);
 
 int *dijkstra(Graph *graph, Node *startNode);
 
