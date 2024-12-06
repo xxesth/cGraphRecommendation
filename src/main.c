48b94c6ddc25716870e39a26aa9ad3dac84dabdf
xesth <muhammedali.karsli1@gmail.com>
2024-12-07 01:29:03 +0300

    updating neural network functions

--- src/main.c
+++ src/main.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "graph.h"
 #include "recommendation.h"
 #include "neuralnetwork.h"
@@ -7,11 +6,11 @@
 #define RECOMMEND_TO_USER 53
 #define RECOMMEND_FOR_ITEM 53
 #define NUMBER_OF_MOVIES 5
+#define EPOCHS 100
 
 int main() {
     Graph *graph = createGraph();
     parseDataFile(graph, DATA_ADDRESS); 
-
     //printGraph(graph);
     
     //Recommendations
@@ -20,22 +19,8 @@ int main() {
     recommendBasedOnSimilarUser(graph, RECOMMEND_TO_USER, NUMBER_OF_MOVIES);
     recommendClosestMovies(graph, RECOMMEND_TO_USER, NUMBER_OF_MOVIES);
     recommendMovieBasedOnItem(graph, RECOMMEND_FOR_ITEM);
-
-    MatrixFactorization *model = initModel(graph);
-    trainModel(model, graph, 100); // 100 epochs
-    float pred = predictRating(model, 53, 53);
-    printf("Predicted rating for User 53, Item 53: %.2f (New prediction)\n", pred);
-    int recommendations[3];
-    // 3 recommendation for user 53
-    getTopNRecommendations(model, graph, 53, 3, recommendations);
-    for (int i = 0; i < 3; i++) {
-        printf("Recommended item %d: Item %d (Predicted rating: %.2f)\n",
-               i + 1, recommendations[i],
-               predictRating(model, 53, recommendations[i]));
-    }
-    freeModel(model);
-
+    recommendNeuralNetwork(graph, EPOCHS, RECOMMEND_TO_USER, RECOMMEND_FOR_ITEM, NUMBER_OF_MOVIES);
+    
     freeGraph(graph);
-
     return 0;
 }
