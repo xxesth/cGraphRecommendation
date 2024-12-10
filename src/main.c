e08f9c5b3607fe6fa3e9a25665291d1dc20e3482
xesth <muhammedali.karsli1@gmail.com>
2024-12-10 23:07:44 +0300

    user input

--- src/main.c
+++ src/main.c
@@ -1,17 +1,28 @@
+#include <stdio.h>
 #include "graph.h"
 #include "recommendation.h"
 #include "neuralnetwork.h"
 
 #define DATA_ADDRESS "./ml-100k/u.data"
-#define RECOMMEND_TO_USER 53
-#define RECOMMEND_FOR_ITEM 53
-#define NUMBER_OF_MOVIES 5
-#define EPOCHS 100
+//#define RECOMMEND_TO_USER 53
+//#define RECOMMEND_FOR_ITEM 53
+//#define NUMBER_OF_MOVIES 5
+//#define EPOCHS 100
 
 int main() {
     Graph *graph = createGraph();
     parseDataFile(graph, DATA_ADDRESS); 
     //printGraph(graph);
+    
+    int RECOMMEND_TO_USER, RECOMMEND_FOR_ITEM, NUMBER_OF_MOVIES, EPOCHS;
+    printf("Enter the ID of the user you want to generate recommendations for (1-943): ");
+    scanf("%d", &RECOMMEND_TO_USER);
+    printf("Enter the ID of the item which will be used for recommendation (1-1682): ");
+    scanf("%d", &RECOMMEND_FOR_ITEM);
+    printf("Enter the number of movies to be recommended (~5): ");
+    scanf("%d", &NUMBER_OF_MOVIES);
+    printf("Enter the number of training epochs (~100): ");
+    scanf("%d", &EPOCHS);
     
     recommendNeuralNetwork(graph, EPOCHS, RECOMMEND_TO_USER, RECOMMEND_FOR_ITEM, NUMBER_OF_MOVIES);
     recommendRandomMovie(graph, RECOMMEND_TO_USER);
